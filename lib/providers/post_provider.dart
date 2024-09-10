265ec4a149a41b6813bdf24371922708fa0b2dfb
Jacob Wilson <jcbwlsn04@gmail.com>
2024-09-10 12:28:06 -0500

    fixed some issues

--- lib/providers/post_provider.dart
+++ lib/providers/post_provider.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:untitled_app/models/firebase_user.dart';
+// import 'package:untitled_app/models/firebase_user.dart';
 
 import 'package:untitled_app/models/post.dart';
 
